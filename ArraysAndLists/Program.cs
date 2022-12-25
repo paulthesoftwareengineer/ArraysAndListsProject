4d7e910bc21a739059fc5e389399fb4084460967
Paul Turner <102317855+paulthesoftwareengineer@users.noreply.github.com>
2022-12-25 07:50:08 +0000

    initial

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -6,27 +6,42 @@ namespace ArraysAndLists
     {
         static void Main(string[] args)
         {
-            //TODO:
+            // Create an array of integers and populate it with the numbers 1-10
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            // Create an int Array and populate numbers 1-10
+            // Create two lists of type int
+            List<int> evens = new List<int>();
+            List<int> odds = new List<int>();
 
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
+            // Use a foreach loop to iterate through the array of numbers
+            foreach (int number in numbers)
+            {
+                // Check if the number is even or odd
+                if (number % 2 == 0)
+                {
+                    // If the number is even, add it to the evens list
+                    evens.Add(number);
+                }
+                else
+                {
+                    // If the number is odd, add it to the odds list
+                    odds.Add(number);
+                }
+            }
 
-            /* Using either a foreach or for loop,
-             * nest an if statement to check to see
-             *  if a number is even or odd.
-             * Then add those numbers to either the evens List
-             * or the odds List
-             */
+            // Display the even numbers using a foreach loop
+            Console.WriteLine("Even numbers:");
+            foreach (int even in evens)
+            {
+                Console.WriteLine(even);
+            }
 
-            /* Now using foreach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
+            // Display the odd numbers using a for loop
+            Console.WriteLine("Odd numbers:");
+            for (int i = 0; i < odds.Count; i++)
+            {
+                Console.WriteLine(odds[i]);
+            }
         }
     }
 }
